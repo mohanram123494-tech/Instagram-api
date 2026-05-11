9e6acd36c64b08b1503138f86939bd88894980b1
mohanram123494-tech <mohanram123494@gmail.com>
2026-05-11 07:06:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const snapsave = require("./snapsave-downloader/src/index");
 const port = 3000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
